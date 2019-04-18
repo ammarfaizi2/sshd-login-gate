f701d5d962ef31d0f3b2fa14d2b052a01e9c7ca3
Ammar Faizi <ammarfaizi2@gmail.com>
2019-04-18 22:32:17 +0700

    Update logger

--- src/classes/Slg/Config.php
+++ src/classes/Slg/Config.php
@@ -20,12 +20,23 @@ final class Config
 		$c = &$_GLOBALS["SSHD_LOGIN_GATE_CONFIG"];
 
 		if (!isset($c)) {
-			printf("SSHD_LOGIN_GATE_CONFIG is not defined!\n");
+			slg_log("SSHD_LOGIN_GATE_CONFIG is not defined!\n");
 			exit(1);
 		}
 
-		if (!isset($c)) {
-			# code...
+		if (!isset($c["log_dir"])) {
+			slg_log("log_dir is not defined!\n");
+			exit(1);
+		}
+
+		if (!isset($c["monitor_bin"])) {
+			slg_log("monitor_bin is not defined!\n");
+			exit(1);
+		}
+
+		if (!isset($c["whois_bin"])) {
+			slg_log("whois_bin is not defined!\n");
+			exit(1);
 		}
 	}
 
--- src/classes/Slg/Daemon.php
+++ src/classes/Slg/Daemon.php
@@ -23,20 +23,29 @@ final class Daemon
 	 */
 	public function __construct()
 	{
+		if (isset($_GLOBALS["SSHD_LOGIN_GATE_CONFIG"]["stdout_log"]) && $_GLOBALS["SSHD_LOGIN_GATE_CONFIG"]["stdout_log"]) {
+			$__global_log_stream[] = fopen("php://stdout", "w");
+		}
+
+		if (getmyuid() != 0) {
+			slg_log("SSHD Login Gate must be run as root!\n");
+			exit(1);
+		}
 
 		if (!function_exists("pcntl_fork")) {
-			printf("pcntl extension required!\n");
+			slg_log("pcntl extension is required!\n");
 			exit(1);
 		}
 
 		if (!function_exists("shmop_open")) {
-			printf("shmop extension required!\n");
+			slg_log("shmop extension is required!\n");
 			exit(1);
 		}
 
 		slg_log("Initializing config...");
 		$this->cfg = new Config;
 
+		slg_log("Config OK!");
 		global $__global_config_var_ref;
 		$__global_config_var_ref = &$this->cfg;
 	}
