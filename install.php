3964d35707e95659a3d72e5b5294df0596c170cb
Ammar Faizi <ammarfaizi2@gmail.com>
2019-04-19 09:54:49 +0700

    Update installer

--- install.php
+++ install.php
@@ -5,6 +5,20 @@ if (trim(shell_exec("whoami")) !== "root") {
 	exit(1);
 }
 
+
+print "Enter bot token: ";
+$token = trim(fread(STDIN, 1024));
+
+print "Enter telegram chat id: ";
+$chatId = trim(fread(STDIN, 1024));
+$config = str_replace(
+	array("{{bot_token}}", "{{chat_id}}"),
+	array($token, $chatId),
+	file_get_contents(__DIR__."/config.php.example")
+);
+
 print shell_exec("/bin/cp -rfv ".escapeshellarg(__DIR__)." /opt/slg");
 print shell_exec("/bin/ln -sfv /opt/slg/slg.service /etc/systemd/system");
-print "\nPlease run `systemctl start slg` to start the service.\n";
+file_put_contents("/opt/slg/config.php", $config);
+print "\n\nPlease run `systemctl start slg` to start the service.\n";
+print "\n\nYou can edit the config at /opt/slg/config.php\n\n";
